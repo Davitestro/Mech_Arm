4d55d6cb06289aa39430a07c4411d7b8902b3c52
Davitestro <147090578+Davitestro@users.noreply.github.com>
2024-06-08 19:40:44 +0400

    Fixing the bug

--- project.ino
+++ project.ino
@@ -65,46 +65,62 @@ void setup() {
 }
 
 void loop() {
+  // Check if the return-to-home button is pressed
   if (!digitalRead(returnToHomePin)) {
-    stepper1.reset();
-    stepper2.reset();
-    stepper3.reset();
-    stepperA.reset();
-    stepperB.reset();
+    // Move all steppers to their initial positions
+    stepper1.setRunMode(FOLLOW_POS);
+    stepper1.setTarget(0);
+    stepper1.tick();
+    
+    stepper2.setRunMode(FOLLOW_POS);
+    stepper2.setTarget(0);
+    stepper2.tick();
+    
+    stepper3.setRunMode(FOLLOW_POS);
+    stepper3.setTarget(0);
+    stepper3.tick();
+    
+    stepperA.setRunMode(FOLLOW_POS);
+    stepperA.setTarget(0);
+    stepperA.tick();
+    
+    stepperB.setRunMode(FOLLOW_POS);
+    stepperB.setTarget(0);
+    stepperB.tick();
+    
     return; // Skip the rest of the loop
   }
 
   /////////////////////////////////////////////////////////////////////////
   if ((digitalRead(30) && digitalRead(31))) {
-    zuygE=0;
+    zuygE = 0;
   } else {
     if (!digitalRead(30) && digitalRead(31)) {
-      zuygL=0;
-      zuygE=1;
+      zuygL = 0;
+      zuygE = 1;
     }
     if (!digitalRead(31) && digitalRead(30)) {
-      zuygL=1;
-      zuygE=1;
+      zuygL = 1;
+      zuygE = 1;
     }
   }
 
   ///////////////////////////////////////////////////////////////////////////////
-  if ((digitalRead(26) && digitalRead(27)) && !zuygE ) {
-    stepperA.reset();
+  if ((digitalRead(26) && digitalRead(27)) && !zuygE) {
     stepperA.setRunMode(KEEP_SPEED);
     stepperA.setSpeed(Av);
     stepperA.setAcceleration(Aa);
   } else {
     if ((!digitalRead(26) && digitalRead(27)) || (zuygL && zuygE)) {
-      if (AL==0) {
-        AL=1;
+      if (AL == 0) {
+        AL = 1;
         stepperA.reverse(AL);
       }
       stepperA.tick();
     }
     if ((!digitalRead(27) && digitalRead(26)) || (!zuygL && zuygE)) {
-      if (AL==1) {
-        AL=0;
+      if (AL == 1) {
+        AL = 0;
         stepperA.reverse(AL);
       }
       stepperA.tick();
@@ -113,22 +129,21 @@ void loop() {
   //////////////////////////////////////////////////////////
 
   //////////////////////////////////////////////////////////////////
-  if ((digitalRead(28) && digitalRead(29)) && !zuygE ) {
-    stepperB.reset();
+  if ((digitalRead(28) && digitalRead(29)) && !zuygE) {
     stepperB.setRunMode(KEEP_SPEED);
     stepperB.setSpeed(Bv);
     stepperB.setAcceleration(Ba);
   } else {
     if ((!digitalRead(28) && digitalRead(29)) || (zuygL && zuygE)) {
-      if (BL==0) {
-        BL=1;
+      if (BL == 0) {
+        BL = 1;
         stepperB.reverse(BL);
       }
       stepperB.tick();
     }
     if ((!digitalRead(29) && digitalRead(28)) || (!zuygL && zuygE)) {
-      if (BL==1) {
-        BL=0;
+      if (BL == 1) {
+        BL = 0;
         stepperB.reverse(BL);
       }
       stepperB.tick();
@@ -138,21 +153,20 @@ void loop() {
   ///////////////////////////////////////////////////////////////////
 
   if (digitalRead(32) && digitalRead(33)) {
-    stepper3.reset();
     stepper3.setRunMode(KEEP_SPEED);
     stepper3.setSpeed(ereqv);
     stepper3.setAcceleration(ereqa);
   } else {
     if (!digitalRead(32) && digitalRead(33)) {
-      if (ereqL==0) {
-        ereqL=1;
+      if (ereqL == 0) {
+        ereqL = 1;
         stepper3.reverse(ereqL);
       }
       stepper3.tick();
     }
     if (!digitalRead(33) && digitalRead(32)) {
-      if (ereqL==1) {
-        ereqL=0;
+      if (ereqL == 1) {
+        ereqL = 0;
         stepper3.reverse(ereqL);
       }
       stepper3.tick();
@@ -161,21 +175,20 @@ void loop() {
   //////////////////////////////////////////////////////////////////
 
   if (digitalRead(22) && digitalRead(23)) {
-    stepper2.reset();
     stepper2.setRunMode(KEEP_SPEED);
     stepper2.setSpeed(erkuv);
     stepper2.setAcceleration(erkua);
   } else {
     if (!digitalRead(22) && digitalRead(23)) {
-      if (erkuL==0) {
-        erkuL=1;
+      if (erkuL == 0) {
+        erkuL = 1;
         stepper2.reverse(erkuL);
       }
       stepper2.tick();
     }
     if (!digitalRead(23) && digitalRead(22)) {
-      if (erkuL==1) {
-        erkuL=0;
+      if (erkuL == 1) {
+        erkuL = 0;
         stepper2.reverse(erkuL);
       }
       stepper2.tick();
@@ -184,21 +197,20 @@ void loop() {
   //////////////////////////////////////////////////////////////////
 
   if (digitalRead(24) && digitalRead(25)) {
-    stepper1.reset();
     stepper1.setRunMode(KEEP_SPEED);
     stepper1.setSpeed(mekv);
     stepper1.setAcceleration(meka);
   } else {
     if (!digitalRead(24) && digitalRead(25)) {
-      if (mekL==0) {
-        mekL=1;
+      if (mekL == 0) {
+        mekL = 1;
         stepper1.reverse(mekL);
       }
       stepper1.tick();
     }
     if (!digitalRead(25) && digitalRead(24)) {
-      if (mekL==1) {
-        mekL=0;
+      if (mekL == 1) {
+        mekL = 0;
         stepper1.reverse(mekL);
       }
       stepper1.tick();
